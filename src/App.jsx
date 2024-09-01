999c107a0d7ed3c3129fe89b7a73e68155ca18a6
JuanRivasFr <156709972+JuanRivasfr@users.noreply.github.com>
2024-09-01 18:30:51 -0500

    feat: :construction: Construccion del portafolio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './css/App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id='section-presentacion'>
+        <div id='container-titulo-descripcion'>
+          <div id='container-img-user'>
+            <img src="/user.jpeg" alt="" />
+          </div>
+          <h1>Juan Rivas WebDev</h1>
+          <small>HTML·CSS·JAVASCRIPT·MONGODB·EXPRESS·NODEJS</small>
+        </div>
+      </section>
+      <section>
+        <p>asdhaskjdhasjk</p>
+      </section>
+    </>
+  )
+}
+
+export default App
