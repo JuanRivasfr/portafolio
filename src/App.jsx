d65b6fd071e965edce4c740e1a72913be787a829
Juan Sebastián Rivas Uribe <156709972+JuanRivasfr@users.noreply.github.com>
2024-09-03 01:25:45 +0000

    feat: :construction: Trabajando en el index

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,13 @@ function App() {
       <section id='section-presentacion'>
         <div id='container-titulo-descripcion'>
           <div id='container-about'>
-            <h1>About Me</h1>
-            <p>Hi! I'm <span> Juan Rivas </span></p>
+            <h1 id='h1-aboutme'>ABOUT ME</h1>
+            <p id='p-h1'>Hi! I'm <span id='span-nombre'> Juan Rivas </span></p>
+            <p id='p-descripcion'>
+              I am a 20-year-old developer with solid knowledge in HTML, CSS, JavaScript, Node.js, Express, and React. 
+              My fascination with programming and eagerness to start my professional career drive me to deliver innovative and efficient solutions. 
+              I am committed to excellence and ready to add value to challenging projects in the field of web development.
+              </p>
           </div>
           <div id='container-img-user'>
             <img src="/user.jpeg" alt="" />
